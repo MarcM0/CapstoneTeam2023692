a8d2b7292a9e6f99f83c5036626c404392a0f252
Marc Morcos <marc.morcos1@hotmail.com>
2023-10-10 22:47:10 -0400

    Comment

--- ConvoAssistant/app/src/main/java/com/example/convoassistant/OpenAIInterfaces.kt
+++ ConvoAssistant/app/src/main/java/com/example/convoassistant/OpenAIInterfaces.kt
@@ -1,24 +1,28 @@
 package com.example.convoassistant
 
-// Template Classes based off of https://platform.openai.com/docs/api-reference/authentication.
+
 class OpenAIUsage(
+    // based off of https://platform.openai.com/docs/api-reference/authentication.
     val prompt_tokens: Number,
     val completion_tokens: Number,
     val total_tokens: Number
 ){}
 
 class OpenAIMessage(
+    // based off of https://platform.openai.com/docs/api-reference/authentication.
     val role: String,
     val content: String
 ){}
 
 class OpenAIChoices(
+    // based off of https://platform.openai.com/docs/api-reference/authentication.
     val message: OpenAIMessage,
     val finish_reason: String,
     val index: Number
 ){}
 
 class OpenAIResponse(
+    // based off of https://platform.openai.com/docs/api-reference/authentication.
     val id: String,
     val created: Number,
     val model: String,
