28ffe2c2e6a56c2c2cca6badbca065b371159e0c
Marc Morcos <marc.morcos1@hotmail.com>
2023-10-28 20:21:30 -0400

    copy over RTA mode stuff to practice mode

--- ConvoAssistant/app/src/main/java/com/example/convoassistant/ui/practice_mode/PracticeModeFragment.kt
+++ ConvoAssistant/app/src/main/java/com/example/convoassistant/ui/practice_mode/PracticeModeFragment.kt
@@ -4,21 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import com.example.convoassistant.R
+import com.example.convoassistant.STTFragment
+import com.example.convoassistant.SettingWrapper
+import com.example.convoassistant.TTSInterfaceClass
 import com.example.convoassistant.databinding.FragmentPracticeModeBinding
+import com.example.convoassistant.makeChatGPTRequest
+import com.example.convoassistant.ui.practice_mode.PracticeModeViewModel
+import kotlin.concurrent.thread
 
 // Practice mode interface
 // Vaguely based on //https://www.geeksforgeeks.org/speech-to-text-application-in-android-with-kotlin/
 
 
-class PracticeModeFragment : Fragment() {
+class PracticeModeFragment : STTFragment(){ // Fragment() { //todo put back to fragment once we do diarization
 
     private var _binding: FragmentPracticeModeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
+    private lateinit var ttsInterface: TTSInterfaceClass
+    private var max_tokens = 50;
+    private var pre_prompt = "";
+
+    //views
+    private lateinit var outputTV: TextView
+    private lateinit var micIB: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,12 +48,54 @@ class PracticeModeFragment : Fragment() {
         _binding = FragmentPracticeModeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // initialize views
+        outputTV =  requireView().findViewById(R.id.speech_2_text_out)
+        micIB = requireView().findViewById(R.id.mic_button)
+
+        // call speech to text when button clicked
+        micIB.setOnClickListener {
+            callSTT()
+        }
+
+        //set up text to speech
+        ttsInterface = TTSInterfaceClass(requireContext())
+
+        val settings = SettingWrapper(requireActivity())
+
+        //TODO change these to be practice mode settings
+        max_tokens = settings.get("RTA_LLM_Output_Token_Count").toInt()
+        pre_prompt = settings.get("RTA_LLM_Prompt")
+
+    }
+
+    override fun onMicResult(input: String){
+        //run in thread so we don't block main
+        thread(start = true) {
+
+            // Run the OpenAI request in a subroutine.
+            val outputText = makeChatGPTRequest(pre_prompt+input,max_tokens)
+
+            // display output text on screen
+            requireActivity().runOnUiThread(Runnable {
+                outputTV.text = (outputText)
+            })
+
+            //text to speech
+            ttsInterface.speakOut(outputText)
+
+
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        ttsInterface.onDestroy()
         _binding = null
     }
 }
